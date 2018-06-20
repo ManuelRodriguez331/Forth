830b16a6273bccea4cc6244ac26b25e218cab59f
ManuelRodriguez331 <32206350+ManuelRodriguez331@users.noreply.github.com>
2018-06-20 05:52:09 +0200

    encapsulated if then

--- fizzbuzz.fs
+++ fizzbuzz.fs
@@ -1,15 +1,31 @@
-: fizz 
-DUP
-3 MOD 0=
-IF ." fizz " 
-ELSE  .
-THEN ;
+#! /usr/bin/gforth
 
 : buzz 
 DUP
 5 MOD 0=
-IF ." buzz " 
+IF DROP ." buzz " 
 ELSE  .
 THEN ;
 
-: main 101 1 do i fizz loop ;
+: fizz 
+DUP
+3 MOD 0=
+IF DROP ." fizz " 
+ELSE buzz
+THEN ;
+
+: fizzandbuzz
+DUP
+15 MOD 0=
+IF DROP ." fizzbuzz " 
+ELSE fizz
+THEN ;
+
+: main
+101 1 do
+i
+fizzandbuzz
+loop ;
+
+main
+bye
