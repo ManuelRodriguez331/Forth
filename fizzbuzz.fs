710ce3b668150ace8f8b8f272d4b6653266c7049
ManuelRodriguez331 <32206350+ManuelRodriguez331@users.noreply.github.com>
2018-06-23 07:06:27 +0200

    nested if

--- fizzbuzz.fs
+++ fizzbuzz.fs
@@ -1,31 +1,37 @@
 #! /usr/bin/gforth
 
-: buzz 
-DUP
-5 MOD 0=
-IF DROP ." buzz " 
-ELSE  .
-THEN ;
+\ title: fizzbuzz
+\ description: prints out the numbers from 1 to 100 and 
+\ determines additionally if the number can be divided by 3 or 5
 
-: fizz 
-DUP
-3 MOD 0=
-IF DROP ." fizz " 
-ELSE buzz
-THEN ;
+\ nested if then cases:
+\ https://www.forth.com/starting-forth/4-conditional-if-then-statements/
 
-: fizzandbuzz
-DUP
-15 MOD 0=
-IF DROP ." fizzbuzz " 
-ELSE fizz
-THEN ;
+variable verbose
+
+: check
+DUP 15 MOD 0= IF ." fizzbuzz " DROP ELSE
+DUP 3 MOD 0= IF ." fizz " DROP ELSE
+DUP 5 MOD 0= IF ." buzz " DROP ELSE
+. \ print number 
+THEN THEN THEN ;
+
+: welcome
+verbose @ \ get variable
+0= if 
+  ." ---------------------------------"
+  CR
+  ." Fizzbuzz test in Forth. Have fun."
+  CR
+  ." ---------------------------------"
+  CR
+then
+;
 
 : main
-101 1 do
-i
-fizzandbuzz
-loop ;
+0 verbose ! \ verbose=true
+welcome
+101 1 do i check loop ;
 
 main
 bye
